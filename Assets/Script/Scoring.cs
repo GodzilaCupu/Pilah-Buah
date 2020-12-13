606b70400173110ac514e4fb1fe266a5c488effc
Gdz.Cp-Pc <eryparasetyo.15@gmail.com>
2020-12-13 10:43:42 +0700

    Stage 1 Complate
    UI+LEVEL CONTROLLER DONE

---
+++ Assets/Script/Scoring.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Scoring : MonoBehaviour
+{
+    public GameObject[] scoreText;
+    public AudioSource audio;
+
+    private void Update()
+    {
+        
+    }
+
+    void Level1()
+    {
+        if(scoreText[0] == true && scoreText[1] == true)
+        {
+
+        }
+    }
+
+    void OnTriggerEnter(Collider bentuk)
+    {
+        if(bentuk.CompareTag("Red"))
+        {
+            audio.Play();
+            scoreText[0].SetActive(true);
+            Destroy(GameObject.FindWithTag("Red"));
+        }
+        if (bentuk.CompareTag("pink"))
+        {
+            audio.Play();
+            scoreText[1].SetActive(true);
+            Destroy(GameObject.FindWithTag("pink"));
+        }
+        if (bentuk.CompareTag("green"))
+        {
+            audio.Play();
+            scoreText[2].SetActive(true);
+            Destroy(GameObject.FindWithTag("green"));
+        }
+    }
+}
